93294e1ae4fce0d6bf815bb1ec2c9e4a38f995c2
Auporna-Barua <aupornabarua@gmail.com>
2023-05-27 22:45:31 -0700

    toys car project full site bootstrapping complate

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import useTitle from "../../Hook/useTitle";
+
+const ErrorPage = () => {
+  useTitle("Error");
+  return (
+    <>
+      <div className="relative">
+        <img
+          className="w-screen h-screen"
+          src="https://i.ibb.co/Yt5wJnp/pngtree-error-page-not-found-404-background-picture-image-1177786.png"
+          alt=""
+        />
+      </div>
+      <div className="absolute top-32 left-3/4 btn text-xl">
+        <Link to={"/"}>
+          <button className="w-60">Go to Home</button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
